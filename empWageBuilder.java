916f3930c513a0db5d8e98ef37de664bb12e8730
AnnaMarina <anna.marina996@gmail.com>
2020-08-27 11:27:29 +0530

    Attendance check

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,5 +1,20 @@
 public class empWageBuilder {
+
+	public static final int FULL_TIME=1;
+	public static final int PART_TIME=2;
 	public static void main(String args[]) {
 		System.out.println("Welcome to Employee Wage Computation");
+		//To check for attendance
+	         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+	         switch(empCheck) {
+	         case FULL_TIME:
+	        	 System.out.println("Full Day Present");
+	        	 break;
+	         case PART_TIME:
+	        	 System.out.println("Half day Present");
+	        	 break;
+	        default:
+	        	System.out.println("Employee is Absent");
+	         }
 	}
 }
