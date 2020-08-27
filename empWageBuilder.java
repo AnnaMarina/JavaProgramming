7ae488402f7519c97d95f15d525a0e45ae98e202
AnnaMarina <anna.marina996@gmail.com>
2020-08-27 11:53:53 +0530

    Daily Employee Wage

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -3,18 +3,22 @@ public class empWageBuilder {
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static void main(String args[]) {
+		int empHours=0, wagePerHour=8;
 		System.out.println("Welcome to Employee Wage Computation");
 		//To check for attendance
 	         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 	         switch(empCheck) {
 	         case FULL_TIME:
-	        	 System.out.println("Full Day Present");
-	        	 break;
+	        	 empHours = 8;
+        		 break;
 	         case PART_TIME:
-	        	 System.out.println("Half day Present");
-	        	 break;
-	        default:
-	        	System.out.println("Employee is Absent");
-	         }
+        		 empHours = 4;
+        		 break;
+        	default:
+        		empHours = 0;
+        	 }
+        	 int empDailyWage = empHours * wagePerHour ;
+        	 System.out.println("Daily Wage : "+empDailyWage);
 	}
+
 }
