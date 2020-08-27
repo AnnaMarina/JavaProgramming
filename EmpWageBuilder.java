8cff8557d1422695c6a8335b8888715ba74cf5a4
AnnaMarina <anna.marina996@gmail.com>
2020-08-28 01:47:07 +0530

    Refactor to arraylist UC13

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -22,43 +22,40 @@ public class EmpWageBuilder {
         		companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
         		System.out.println(companyEmpWage);
         	}
-                 }
+        }
         public int computeEmpWage(CompanyEmpWage companyEmpWage) {
         	int totalWage=0, empHours=0, days=0, totalEmpHours=0, empDailyWage=0;
-            System.out.println("Welcome to Employee Wage Computation \n");
-            System.out.println("*****DAILY WAGE COMPUTAION****** \n");
-            while(days < companyEmpWage.workingDays  && totalEmpHours < companyEmpWage.maxHours ) {
-                    int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-                    switch(empCheck) {
-                            case FULL_TIME:
-                                    empHours = 8;
-                                    //System.out.println("Employee present for full day");
-                                    break;
-                            case PART_TIME:
-                                    empHours = 4;
-                                    //System.out.println("Employee present for half day");
-                                    break;
-                            default:
-                                    empHours = 0;
-                                    //System.out.println("Employee Absent");
-                    }
-                    empDailyWage = empHours * companyEmpWage.wagePerHour ;
-                    totalEmpHours+=empHours;
-                    days+=1;
-                    System.out.println("Wage for day "+days+" : "+empDailyWage);
-                    totalWage+=empDailyWage;
-            }
-            return totalWage;
-
+        	System.out.println("Welcome to Employee Wage Computation \n");
+        	System.out.println("*****DAILY WAGE COMPUTAION****** \n");
+        	while(days < companyEmpWage.workingDays  && totalEmpHours < companyEmpWage.maxHours ) {
+                	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+                    	switch(empCheck) {
+                        	case FULL_TIME:
+                                    	empHours = 8;
+                                    	//System.out.println("Employee present for full day");
+                                    	break;
+                            	case PART_TIME:
+                                    	empHours = 4;
+                                    	//System.out.println("Employee present for half day");
+                                    	break;
+                            	default:
+                                   	empHours = 0;
+                                   	//System.out.println("Employee Absent");
+                	}
+                	empDailyWage = empHours * companyEmpWage.wagePerHour ;
+                	totalEmpHours+=empHours;
+                	days+=1;
+			//Prints daily wage
+                	System.out.println("Wage for day "+days+" : "+empDailyWage);
+                	totalWage+=empDailyWage;
+		}
+            	return totalWage;
         }
-
 
         public static void main(String args[]) {
-        EmpWageBuilder empWageBuilder = new EmpWageBuilder();
-        empWageBuilder.addCompanyEmpWage("More",8,20,100);
-        empWageBuilder.addCompanyEmpWage("KR Bakers",10,25,100);
-        empWageBuilder.computeEmpWage();
+        	EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+        	empWageBuilder.addCompanyEmpWage("More",8,20,100);
+        	empWageBuilder.addCompanyEmpWage("KR Bakers",10,25,100);
+        	empWageBuilder.computeEmpWage();
         }
-
-
 }
