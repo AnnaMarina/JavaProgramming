799853b6de64d24df0fa374e64428d28df9f983d
AnnaMarina <anna.marina996@gmail.com>
2020-08-27 12:37:16 +0530

    Monthly wage calculated

--- empWageBuilder.java
+++ empWageBuilder.java
@@ -1,28 +1,30 @@
 public class empWageBuilder {
-
 	public static final int FULL_TIME=1;
 	public static final int PART_TIME=2;
 	public static void main(String args[]) {
-		int empHours=0, wagePerHour=8;
+		int empHours=0, wagePerHour=8, workingdays=20, totalWage=0, d=0;
 		System.out.println("Welcome to Employee Wage Computation");
-		//To check for attendance
-	         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	         switch(empCheck) {
-	         case FULL_TIME:
-			System.out.println("Employee present for full day");
-	        	empHours = 8;
+         do {
+        	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        	workingdays-=1;
+        	switch(empCheck) {
+        	case FULL_TIME:
+        		empHours = 8;
+        		//System.out.println("Employee present for full day");
         		break;
-		//partime
-	         case PART_TIME:
-			System.out.println("Employee present for half day");
+        	case PART_TIME:
         		empHours = 4;
+        		//System.out.println("Employee present for half day");
         		break;
         	default:
-			System.out.println("Employee Absent");
         		empHours = 0;
-        	 }
-        	 int empDailyWage = empHours * wagePerHour ;
-        	 System.out.println("Daily Wage : "+empDailyWage);
+        		//System.out.println("Employee Absent");
+        	}
+        	int empDailyWage = empHours * wagePerHour ;
+        	totalWage+=empDailyWage;
+        	d+=1;
+        	System.out.println("Wage for day "+d+" : "+empDailyWage);
+         	} while(workingdays > 0);
+         System.out.println("The total wage is : "+totalWage);
 	}
-
 }
