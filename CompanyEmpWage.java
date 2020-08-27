f8a656543a2ecbe6eeae18e443da94fd261552f6
AnnaMarina <anna.marina996@gmail.com>
2020-08-27 23:31:37 +0530

    Wage computation using array

---
+++ CompanyEmpWage.java
@@ -0,0 +1,24 @@
+package Empwage;
+
+public class CompanyEmpWage {
+		public final String company;
+		public final int maxHours;
+		public final int workingDays;
+		public final int wagePerHour;
+		public int totalWage;
+
+	    public CompanyEmpWage(String company, int wagePerHour, int workingDays, int maxHours) {
+	               this.company = company;
+	               this.wagePerHour = wagePerHour;
+	               this.workingDays = workingDays;
+	               this.maxHours = maxHours;
+	    }
+	    public void setTotalEmpWage(int totalWage ) {
+	    	this.totalWage = totalWage;
+	    }
+
+	    public String toString() {
+	        System.out.println("\n**************EMPLOYEE DETAILS**************");
+	        return " COMPANY NAME  : "+company+"\n MONTHLY WAGE  : "+totalWage+"/- \n";
+	}
+}
